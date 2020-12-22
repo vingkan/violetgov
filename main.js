fe62a558d811dff918c40b094aa2da2ee8d363c4
Vinesh Kannan <vinesh@omnipointment.com>
2020-12-22 13:51:43 -0600

    Add and style user component.

--- main.js
+++ main.js
@@ -20,15 +20,45 @@ function parseUsersCSV(csv) {
     return users;
 }
 
+class User extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const user = this.props.user;
+        const firstInitial = user.FirstName.length >= 1
+            ? user.FirstName.substr(0, 1).toUpperCase()
+            : "?";
+        const displayName = `${user.FirstName} ${user.LastName}`;
+        return (
+            <div className="User">
+                <div className="User__Initial">{firstInitial}</div>
+                <div className="User__Profile">
+                    <h3>{displayName}</h3>
+                    <p>{user.Email || "No Email"}</p>
+                    <p>{user.ZipCode || "No Zip Code"}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
 class Main extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
+        const users = this.props.users;
         return (
-            <div>
+            <div className="Main">
                 <h1>Welcome to VioletGov!</h1>
                 <p>Helping you build strong constituent relationships.</p>
+                <h2>Your Constituents</h2>
+                <div className="Main__Users">
+                    {users.map((user, i) => {
+                        return <User key={i} user={user} />;
+                    })}
+                </div>
             </div>
         );
     }
@@ -36,6 +66,7 @@ class Main extends React.Component {
 
 /*
  * Red is missing LastName
+ * Purple is missing Email
  * Yellow is missing ZipCode
  * Green and Blue have non-ASCII characters
  * Blank lines should not produce users
@@ -48,10 +79,10 @@ orange@colors.us,Orange,O'Brien,60623
 yellow@colors.us,Yellow,Yesenia,
 green@colors.us,Green,González,60616
 blue@colors.us,Blue,Bueños,60607
+,Purple,Postovsky,60622
 `;
 
 const usersJSON = parseUsersCSV(usersCSV);
-console.log(usersJSON);
 
 const mainEl = <Main users={usersJSON} />
 ReactDOM.render(mainEl, document.getElementById("main"));
